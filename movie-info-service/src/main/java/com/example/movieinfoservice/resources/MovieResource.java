fdf633a1d246fac8944d96cfb7b28e0dc189b8e1
BeedoProgrammer <abdelrahmanh38@gmail.com>
2026-04-08 23:39:48 +0200

    Finished mongoDB part

--- movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
@@ -2,12 +2,15 @@ package com.example.movieinfoservice.resources;
 
 import com.example.movieinfoservice.models.Movie;
 import com.example.movieinfoservice.models.MovieSummary;
+import com.example.movieinfoservice.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movies")
@@ -17,17 +20,31 @@ public class MovieResource {
     private String apiKey;
 
     private RestTemplate restTemplate;
+    private MovieRepository movieRepository;
 
-    public MovieResource(RestTemplate restTemplate) {
+    public MovieResource(RestTemplate restTemplate, MovieRepository movieRepository) {
         this.restTemplate = restTemplate;
+        this.movieRepository = movieRepository;
     }
 
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-        // Get the movie info from TMDB
+        
+        // Check if the movie is in the database first
+        Optional<Movie> cachedMovie = movieRepository.findById(movieId);
+        if (cachedMovie.isPresent()) {
+            return cachedMovie.get();
+        }
+
+        // Get the movie info from TMDB (Original code)
         final String url = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey;
         MovieSummary movieSummary = restTemplate.getForObject(url, MovieSummary.class);
 
-        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+        Movie movie = new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+        
+        // Save to database
+        movieRepository.save(movie);
+
+        return movie;
     }
 }
