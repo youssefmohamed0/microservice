47fda56b545261f8c5e3fbf2ec8a807782c0ea04
Abdelrahman Amr <boody2205@gmail.com>
2026-04-08 06:18:52 +0200

    Finished changing the rating service source of data to a mysql database. MySQL configuration still needs to be done

---
+++ ratings-data-service/src/main/java/com/example/ratingsservice/repositories/RatingRepository.java
@@ -0,0 +1,13 @@
+package com.example.ratingsservice.repositories;
+
+import com.example.ratingsservice.models.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+    List<Rating> findByUserId(String userId);
+
+}
